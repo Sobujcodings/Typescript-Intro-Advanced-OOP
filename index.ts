50cd15b9b4409cb34cf75a7a904fe572163aac0b
merajsobuj <merajsobuj@gmail.com>
2025-05-08 23:34:19 +0600

    assignment-1

--- index.ts
+++ index.ts
@@ -1,42 +1,32 @@
 function formatString(input: string, toUpper?: boolean) {
   if (toUpper === true || toUpper === undefined) {
     const lowerCase = input.toUpperCase();
-    console.log(lowerCase);
     return lowerCase;
   } else {
     const upperCase = input.toLowerCase();
-    console.log(upperCase);
     return upperCase;
   }
 }
 
-
 function filterByRating(
   items: { title: string; rating: number }[]
-): { title: string; rating: number }[] 
-{
+): { title: string; rating: number }[] {
   const filteredItems = items.filter((item) => item.rating >= 4);
-  console.log(filteredItems); 
   return filteredItems;
 }
-
 
 function concatenateArrays<T>(...arrays: T[][]) {
   let result: T[] = [];
   for (const array of arrays) {
     result = result.concat(array);
   }
-  console.log(result);
   return result;
 }
-
-
 
 class Vehicle {
   private make: string;
   private year: number;
   getInfo() {
-    console.log(`"Make: ${this.make}, Year: ${this.year}"`);
     return `"Make: ${this.make}, Year: ${this.year}"`;
   }
   constructor(make: string, year: number) {
@@ -47,7 +37,6 @@ class Vehicle {
 class Car extends Vehicle {
   private model: string;
   getModel() {
-    console.log(`"Model: ${this.model}"`);
     return `"Model: ${this.model}"`;
   }
   constructor(make: string, year: number, model: string) {
@@ -56,21 +45,15 @@ class Car extends Vehicle {
   }
 }
 
-
-
 function processValue(value: string | number): number {
   if (typeof value === "string") {
-    console.log(value.length);
     return value.length;
   } else if (typeof value === "number") {
-    console.log(value * 2);
     return value * 2;
   } else {
     throw new Error("Invalid Input: should be a string or number");
   }
 }
-
-
 
 interface Product {
   name: string;
@@ -86,11 +69,8 @@ function getMostExpensiveProduct(products: Product[]): Product | null {
       max = product;
     }
   });
-  console.log(max);
   return max;
 }
-
-
 
 enum Day {
   Monday,
@@ -103,21 +83,14 @@ enum Day {
 }
 
 function getDayType(day: Day): string {
-  let result: string;
-
   switch (day) {
     case Day.Saturday:
     case Day.Sunday:
-      result = "Weekend";
-      break;
+      return "Weekend";
     default:
-      result = "Weekday";
+      return "Weekday";
   }
-  console.log(result);
-  return result;
 }
-
-
 
 async function squareAsync(params: number): Promise<number> {
   return new Promise<number>((resolve, reject) => {
